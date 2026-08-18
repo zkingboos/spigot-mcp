4317981f21da52799380d0e5514306b5b01ef744
Samuel Henrique <150966678+zyypj@users.noreply.github.com>
2026-08-18 16:07:20 -0300

    refactor: make position and config types version-agnostic

--- src/main/kotlin/xyz/joseg/spigotmcp/util/Position.kt
+++ src/main/kotlin/xyz/joseg/spigotmcp/util/Position.kt
@@ -1,6 +1,5 @@
 package xyz.joseg.spigotmcp.util
 
-import com.sk89q.worldedit.math.BlockVector3
 import org.bukkit.World
 
 data class Pos(
@@ -9,13 +8,30 @@ data class Pos(
     val z: Int,
     val world: String
 ) {
-    fun toBlockVector3(): BlockVector3 = BlockVector3.at(x, y, z)
-    
-    fun toBukkitLocation(world: World) = org.bukkit.Location(world, x.toDouble(), y.toDouble(), z.toDouble())
-    
+    fun toBukkitLocation(bukkitWorld: World) =
+        org.bukkit.Location(bukkitWorld, x.toDouble(), y.toDouble(), z.toDouble())
+
+    fun offset(dx: Int = 0, dy: Int = 0, dz: Int = 0) = copy(x = x + dx, y = y + dy, z = z + dz)
+}
+
+data class Cuboid(val min: Pos, val max: Pos) {
+
+    val world: String get() = min.world
+
+    val volume: Long
+        get() = (max.x - min.x + 1).toLong() *
+            (max.y - min.y + 1).toLong() *
+            (max.z - min.z + 1).toLong()
+
     companion object {
-        fun fromBlockVector3(vec: BlockVector3, worldName: String): Pos = Pos(
-            x = vec.getBlockX(), y = vec.getBlockY(), z = vec.getBlockZ(), world = worldName
-        )
+        fun of(a: Pos, b: Pos): Cuboid {
+            require(a.world == b.world) {
+                "pos1 and pos2 must be in the same world (got '${a.world}' and '${b.world}')"
+            }
+            return Cuboid(
+                min = Pos(minOf(a.x, b.x), minOf(a.y, b.y), minOf(a.z, b.z), a.world),
+                max = Pos(maxOf(a.x, b.x), maxOf(a.y, b.y), maxOf(a.z, b.z), a.world)
+            )
+        }
     }
 }
